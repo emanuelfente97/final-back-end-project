da6b1267f2d7634b2bbef07939ce81ce9bbf2351
emanuelfente97 <emanuelfente97@gmail.com>
2022-03-22 14:56:19 +0200

    removed env

--- routes/contact.js
+++ routes/contact.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 const router = require("express").Router();
-require('dotenv').config();
+// require('dotenv').config();
 
-router.get('/',(req, res) => res.send({ msg: 'send contact using POST' }));
+router.get("/", (req, res) => res.send({ msg: "send contact using POST" }));
 
-router.post('/',(req, res) => {
+router.post("/", (req, res) => {
   const { name, email, message, subject } = req.body;
   let transporter = nodemailer.createTransport({
-
-    service: 'gmail',
-    host: 'smtp.gmail.com',
+    service: "gmail",
+    host: "smtp.gmail.com",
     port: 465,
-    secure:true,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
@@ -20,19 +19,19 @@ router.post('/',(req, res) => {
   });
   var mailOptions = {
     from: email,
-    to: 'emanuelfente97@gmail.com',
+    to: "emanuelfente97@gmail.com",
     subject: `${subject}`,
     text: `${name} has messaged you saying: ${message}`,
   };
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
-      res.status(400).send({ msg: 'Email could not be sent' + error });
+      res.status(400).send({ msg: "Email could not be sent" + error });
     } else {
-      console.log('Email sent: ' + info.response);
-      res.send({ msg: 'Message sent succesfully' });
+      console.log("Email sent: " + info.response);
+      res.send({ msg: "Message sent succesfully" });
     }
   });
-})
+});
 
 module.exports = router;
