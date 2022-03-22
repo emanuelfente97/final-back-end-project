97b7f5ce26e18ecd26eb28577050586e293bc25b
emanuelfente97 <emanuelfente97@gmail.com>
2022-03-22 14:19:55 +0200

    upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const authRoute = require("./routes/auth");
-const ContactRoute = require("./routes/contact")
+const ContactRoute = require("./routes/contact");
 
 mongoose
   .connect(process.env.DATA_URL, { useNewUrlParser: true })
@@ -19,6 +19,10 @@ app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/contacts", ContactRoute);
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend serve is running ");
